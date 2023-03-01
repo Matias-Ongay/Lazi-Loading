c051aaed0ebf69b7df4a356eaf7ce92f5358c516
Matias-Ongay <ongaymatias@gmail.com>
2023-03-01 10:06:40 -0300

    Workshop 2,lazi loading finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from './lazy';
 
-console.log('Happy hacking :)')
+const maximum=122;
+const minimum=1;
+const random = () =>Math.floor(Math.random()*(maximum-minimum))+minimum;
+
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className="p-4";
+    const imagen= document.createElement("img");
+    imagen.className="mx-auto";
+    imagen.width= '320';
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+    container.appendChild(imagen)
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode =document.getElementById("images");
+
+const addButton = document.getElementById("agregar");
+const deleteButton =document.getElementById("eliminar");
+
+const addImage = () => {
+    const newImage=createImageNode();
+    mountNode.appendChild(newImage);
+    registerImage(newImage);
+};
+addButton.addEventListener("click",addImage);
+
+
+const deleteImage = () =>{
+    [...mountNode.childNodes].forEach(child => {
+        child.remove();
+    }) 
+}
+deleteButton.addEventListener("click",deleteImage);
